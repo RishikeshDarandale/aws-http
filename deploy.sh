53e83906dcf09de8ab582cffbf8dab71700f0fa3
Rishikesh Darandale <Rishikesh.Darandale@gmail.com>
2018-02-20 01:24:44 +0530

    Changing the steps of travis build

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_22bbbead7bbb_key -iv $encrypted_22bbbead7bbb_iv -in my.travis.gpg.enc -out my.travis.gpg -d
   ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
-else
-  ./gradlew assemble -x signArchives
 fi
